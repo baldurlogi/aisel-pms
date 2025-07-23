798c5967beeb024c1b407aa60117dc240230a9df
BaldurLogi <baldurlogi16@icloud.com>
2025-07-23 18:15:27 +0200

    chore: define node & package manager and add linters & formatters

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+// const ts = require('@typescript-eslint/eslint-plugin');
+// const tsParser = require('@typescript-eslint/parser');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const imp = require('eslint-plugin-import');
+const n = require('eslint-plugin-n');
+const prettier = require('eslint-plugin-prettier');
+
+/** @type {import('eslint').FlatConfig[]} */
+module.exports = [
+  // --------------------------------------------------
+  // TypeScript + React
+  // --------------------------------------------------
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      // parser: tsParser,
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: true },
+    },
+    plugins: {
+      // '@typescript-eslint': ts,
+      react,
+      'react-hooks': reactHooks,
+      import: imp,
+      n,
+      prettier,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      //...ts.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...imp.configs.recommended.rules,
+      ...n.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+  // Jest / vitest tests
+  {
+    files: ['**/*.test.{ts,tsx,js,jsx}'],
+    languageOptions: { globals: { jest: 'readonly', vi: 'readonly' } },
+  },
+];
