7e403e4c27bd4e76d0bbb7a9f596df2d79c0a2f0
BaldurLogi <baldurlogi16@icloud.com>
2025-07-25 18:30:40 +0200

    fix: github actions ci

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -28,7 +28,7 @@ export default tseslint.config(
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
