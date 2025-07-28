0b1e234777ba1570d77f2f55508ee72a20da6787
BaldurLogi <baldurlogi16@icloud.com>
2025-07-28 03:10:11 +0200

    fix: deployment for railway

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -10,3 +10,15 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+@Controller()
+export class DebugController {
+  @Get('debug/env')
+  debugEnv() {
+    return {
+      JWT_SECRET_defined: !!process.env.JWT_SECRET,
+      // optionally: length to be extra sure
+      JWT_SECRET_length: process.env.JWT_SECRET?.length ?? 0,
+    };
+  }
+}
