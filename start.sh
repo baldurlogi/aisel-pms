68d0424e84836d58549312dff78c7b595f44bcff
BaldurLogi <baldurlogi16@icloud.com>
2025-07-28 18:32:53 +0200

    fix: deployment

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "🌱 Running Prisma db push..."
 pnpm --filter api prisma db push --schema=apps/api/src/prisma/schema.prisma
 
 echo "🚀 Starting app..."
-node dist/main.js
+node apps/api/dist/main.js
